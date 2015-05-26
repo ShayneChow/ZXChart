20744d04591b4a9f1dd0ccbb6493195eb42b0bea
Shayne Chow <shayne@authbuy.org>
2015-05-26 13:19:07 +0800

    Initial Commit

---
+++ ZXChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZXChart
+//
+//  Created by ChowShayne on 15/5/26.
+//  Copyright (c) 2015年 微加科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
